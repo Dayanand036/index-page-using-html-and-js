5b315bc498a4e25a7f7497bb9c17516ce268416c
Dayanand036 <147906796+Dayanand036@users.noreply.github.com>
2023-10-22 10:18:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+// function mul(n1,n2,n3,n4) {
+ //   return n1 * n2 * n3 * n4;
+//}
+
+document.write(mul(4,5,6,7));
+
+// function greet() {
+//    alert("Hello Everyone");
+// }
+
+// let age= 10;
+
+// if(age >=18) {
+//   document.write("Eligible for vote");
+// } else {
+//   document.write("Not Elligible");
+// }
+
+// for (let num = 10; num>=1; num--) {
+//    console.log(num);
+//}
+
+
+// Dom - Document object model
+// Dom Manipulation Methods
+// document.getElementById()
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// document.querySelector()
+// document.querySelectorAll()
+
+
+// document.getElementById("head").innerHTML;
+// console.log(el);
+
+let ele = document.querySelector("#head").innerHTML;
+console.log(ele);
